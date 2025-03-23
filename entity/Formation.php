c13e5c3d086eba24a57df414eddd06b8532df537
theokachlergit <116169255+TheoGhostKuro@users.noreply.github.com>
2025-03-24 00:43:30 +0100

    formation getFormation

--- entity/Formation.php
+++ entity/Formation.php
@@ -3,6 +3,12 @@ class Formation
 {
     private $idFormation;
     private $libelleFormation;
-    public function __construct()    {
+    public function __construct() {}
+
+    public function getFormation($pdo)
+    {
+        $statement = $pdo->prepare("SELECT * FROM formation");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 }
